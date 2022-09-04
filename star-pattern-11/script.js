bc210cea202766dd9510592022d0c9bc4e32d1a2
Shahzada-Faizi <devfaizi435@gmail.com>
2022-09-03 22:23:39 -0700

    star-pattern-11

---
+++ star-pattern-11/script.js
@@ -0,0 +1,9 @@
+var i;
+var j;
+var k = 6;
+for (i = 1; i <= 5; i++) {
+    for (j = 1; j <= k - i; j++) {
+        document.write("*");
+    }
+    document.write("<br>");
+}
---
+++ star-pattern-11/script.ts
@@ -0,0 +1,9 @@
+let i:number;
+let j:number;
+let k:number = 6;
+for(i=1;i<=5;i++){
+    for(j=1;j<=k-i;j++){
+        document.write("*")
+    }
+    document.write("<br>")
+}
